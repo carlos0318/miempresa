45cb4390a7e2656d3f7141b5903ef04eb6f4df9d
Carlos <carlos.ortega.ast0318@gmail.com>
2020-03-07 15:30:16 -0600

    examen

---
+++ layoutEmpresa.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="estilos/empresa.css">
+
+</head>
+
+<body bgcolor="#D8DED9">
+    <form action="">
+        <center>
+            <table bgcolor="white">
+                <tr>
+                    <td align="left">
+                        <img src="imágenes/logo.png" alt="">
+                    </td>
+                    <td>
+                        <font class="fontTitulo1"><b>Emprender</b></font> <br>
+                        <font><b>Soluciones de empleo</b></font>
+                    </td>
+                    <td>
+                        <td class="navegador">
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="#">Buscas empleo</a></li>
+                            <li><a href="#">Empleadores</a></li>
+                            <li><a href="#">Quines somos</a></li>
+                        </td>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <table>
+                            <tr>
+                                <td>
+                                    <img src="imágenes/img1.jpg" class="fontImg">
+                                    <img src="imágenes/img2.jpg" class="fontImg">
+                                    <center>
+                                        <table border="0" class="cuadrop">
+                                            <tr>
+                                                <td>
+                                                    <font class="fontTitulo2"><b>¡Proporcionamos a cientos de</b></font><br>
+                                                    <font class="fontTitulo2"><b>empleados cada mes!</b></font><br>
+
+                                                </td>
+                                            </tr>
+
+                                        </table>
+                                        <table class="cuadroB" border="0">
+                                            <tr>
+                                                <td>
+                                                <input type="button" value="¿Buscas Trabajo?" class="botonP">
+                                                <input type="button" value="¿Buscas Contratar?"class="botonC">
+                                                </td>
+                                            </tr>
+                                        </table>
+                                        <table border="0" class="cuadrob">
+                                            <tr>
+                                                <td>
+                                                    <br>
+                                                    <font class="fontTitulo3"><b>Estamos aquí para lo que sea que esté buscando</b></font><br>
+                                                    <br>
+                                                    <font class="fontTitulo4">Parrafo. Haz clic para editar y agregar tu propio texto. Es fácil. Haz clic en Editar Texto o doble clic para agregar tu contenido y cambiar la <br> 
+                                                    fuente. Puedes arrastrar y soltar este texto donde sea que quieras en tu pagina. En este espacio puedes contar tu historia y permitir …….
+                                                    </Parrafo.></font><br>
+                                                </td>
+
+                                            </tr>
+                                        </table>
+                                    </center>
+                                    </td>
+                            </tr>
+                        </table>
+                        </td>
+
+                </tr>
+
+            </table>
+
+        </center>
+        <CENter>
+        <table class="cuadroa" >
+                <tr>
+                    <td align="center" bgcolor="#D8DED9">
+                        <font class="fontEtiqueta">
+                            Derechos reservados
+                        </font>
+                        &nbsp;
+                        <a href="" target="_blank"><img src="imágenes/gl.png" class="imgRedes"></a> &nbsp;
+                        <a href="https://www.facebook.com" target="_blank"> <img src="imágenes/fb.png" class="imgRedes"></a> &nbsp;
+                    </td>
+                </tr>
+            </table>
+        </CENter>
+            
+    </form>
+
+</body>
+
+</html>
